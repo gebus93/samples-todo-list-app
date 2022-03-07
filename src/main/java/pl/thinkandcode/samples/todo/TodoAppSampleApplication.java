facb63e0798ec199873e462c3024e345535f40bf
gebus93 <gebus.93@gmail.com>
2022-03-07 20:25:29 +0100

    add spring-boot application (#1)

---
+++ src/main/java/pl/thinkandcode/samples/todo/TodoAppSampleApplication.java
@@ -0,0 +1,13 @@
+package pl.thinkandcode.samples.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppSampleApplication.class, args);
+	}
+
+}
