7daf847138ef2a608bc6fa98763fe5d55b6e1ac6
Łukasz Gębicki <gebus.93@gmail.com>
2022-03-07 20:24:05 +0100

    add spring-boot application

---
+++ src/test/java/pl/thinkandcode/samples/todo/TodoAppSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.thinkandcode.samples.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
