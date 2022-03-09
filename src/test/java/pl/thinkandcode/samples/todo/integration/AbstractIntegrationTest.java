6da17d721384eb688f1e044cf8c59e86bab5682d
Łukasz Gębicki <gebus.93@gmail.com>
2022-03-09 11:17:47 +0100

    add identifiers adapter

---
+++ src/test/java/pl/thinkandcode/samples/todo/integration/AbstractIntegrationTest.java
@@ -0,0 +1,9 @@
+package pl.thinkandcode.samples.todo.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.thinkandcode.samples.todo.annotations.IntegrationTest;
+
+@SpringBootTest
+@IntegrationTest
+public class AbstractIntegrationTest {
+}
